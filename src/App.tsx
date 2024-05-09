350161f1f84fd9a19d8d7f694c04dad850bed4ab
sachin supunthaka <sachinsupunthaka96@gmail.com>
2024-05-09 09:31:49 +0530

    chor: min changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Classes from "./components/Page/Classes";
 import Revision from "./components/Page/Revision";
 import Teachers from "./components/Page/Teachers";
-// import Navbar from "./components/navbar/Navbar";
-// import Sidebar from "./components/sidebar/Sidebar";
+
 import ShoppingCart from "./components/Page/ShoppingCart";
-// import TotalComponents from "../TotalComponent";
+
 import RevitionLimit from "./components/Page/RevitionLimit";
 import Profile from "./components/Page/Profile";
 import Note from "./components/Page/Note";
