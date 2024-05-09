6647233ae3f77e6abebb35f32897dbe77586e5df
sachin supunthaka <sachinsupunthaka96@gmail.com>
2024-05-09 10:37:18 +0530

    chor: adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,21 @@ import Login from "./pages/login";
 import Profile from "./pages/profile";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/register";
+import Classes from "./components/Page/Classes";
+import Revision from "./components/Page/Revision";
+import Teachers from "./components/Page/Teachers";
+import ShoppingCart from "./components/Page/ShoppingCart";
+import RevitionLimit from "./components/Page/RevitionLimit";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/classes" element={<Classes />} />
+        <Route path="/revision" element={<Revision />} />
+        <Route path="/teachers" element={<Teachers />} />
+        <Route path="/shoppingCart" element={<ShoppingCart />} />
+        <Route path="/revisionLimit" element={<RevitionLimit />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/" element={<Login />} />
