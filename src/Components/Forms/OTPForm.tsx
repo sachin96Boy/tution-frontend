533a99eaf908a9cb414e2422484ad5adad436d4b
Dilshan Hashitha <hashithadiljay@gmail.com>
2024-05-13 15:47:55 +0530

    OTP validation implemented

--- src/Components/Forms/OTPForm.tsx
+++ src/Components/Forms/OTPForm.tsx
@@ -3,6 +3,7 @@ import otp_clipArt from "../../assets/Images/OTP_clipArt.png";
 import { useState } from "react";
 const OTPForm = () => {
   const [OTP, setOTP] = useState("");
+  const [error, setError] = useState("");
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const phone = "9876543210";
@@ -17,6 +18,9 @@ const OTPForm = () => {
       </h1>
       <p className="font-[500] font-montserrat text-[15px] text-prime-alt max-w-[100%] w-[330px] text-center">
         Enter the OTP sent to <span className="font-[700]">{phone}</span>
+      </p>
+      <p className="font-[500] font-montserrat text-[12px] text-tertiary-alt text-center">
+        {error}
       </p>
       <OTPInput
         value={OTP}
@@ -42,6 +46,8 @@ const OTPForm = () => {
           e.preventDefault();
           if (OTP.length == 4) {
             console.log(OTP);
+          } else {
+            setError("Please Enter Correct OTP");
           }
         }}
         className=" mt-4 rounded-[5px] bg-tertiary-alt text-second-alt font-montserrat text-[12px] max-w-[100%] font-[700] w-[396px] h-[50px]"
